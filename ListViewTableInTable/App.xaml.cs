5f337e50302d8b030fc5db1c967a65602f820623
c3n9 <arseniy11041104@gmail.com>
2023-06-14 17:49:34 +0300

    listview table in table

---
+++ ListViewTableInTable/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ListViewTableInTable.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ListViewTableInTable
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static NyamNyamEntities DB = new NyamNyamEntities();
+    }
+}
